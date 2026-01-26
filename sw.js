fda9e4748ce9cef11d162f6b851bd83d559e9aed
Oleksii Zhuravko <115608102+oleksii-zhuravko@users.noreply.github.com>
2026-01-26 13:56:36 +0200

    Update service worker cache version to v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'scanner412-v9';
+const CACHE_NAME = 'scanner412-v10';
 
 const ASSETS = [
   './',
@@ -34,6 +34,7 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
 
 
